e9222a325e1b5b3da7b8196036d1ab1393818746
athishnatdemy-collab <athishnatdemy@gmail.com>
2026-09-17 14:38:17 +0530

    refactor: inline thumbnail generation in backfill command to support remote storage

--- gallery/management/commands/backfill_thumbnails.py
+++ gallery/management/commands/backfill_thumbnails.py
@@ -1,8 +1,9 @@
 from django.core.management.base import BaseCommand
 from gallery.models import GalleryImage
-from gallery.tasks import create_thumbnail
 from django.core.files.base import File
 import os
+from io import BytesIO
+from PIL import Image, ImageOps
 
 class Command(BaseCommand):
     help = 'Backfill missing thumbnails for images imported via Google Drive.'
@@ -15,18 +16,29 @@ class Command(BaseCommand):
         
         for i, img in enumerate(images, 1):
             try:
-                full_img_path = img.file.path
-                if not os.path.exists(full_img_path):
-                    self.stdout.write(self.style.ERROR(f"[{i}/{total}] Skipping {img.id} - File missing on disk."))
-                    continue
-                    
-                thumb_io, ext = create_thumbnail(full_img_path)
-                if thumb_io:
-                    thumb_name = os.path.splitext(img.filename)[0] + "_thumb" + ext
-                    img.thumbnail.save(thumb_name, File(thumb_io), save=True)
-                    self.stdout.write(self.style.SUCCESS(f"[{i}/{total}] Created thumbnail for {img.filename}"))
-                else:
-                    self.stdout.write(self.style.ERROR(f"[{i}/{total}] Failed to generate thumbnail for {img.filename}"))
+                # Use img.file directly to support S3 / remote backends
+                with img.file.open('rb') as f:
+                    with Image.open(f) as pil_img:
+                        pil_img = ImageOps.exif_transpose(pil_img)
+                        pil_img.thumbnail((600, 600), Image.Resampling.LANCZOS)
+                        
+                        thumb_io = BytesIO()
+                        ext = os.path.splitext(img.filename)[1].lower()
+                        
+                        fmt = 'JPEG'
+                        if ext in ['.png']: fmt = 'PNG'
+                        elif ext in ['.webp']: fmt = 'WEBP'
+                        
+                        if pil_img.mode in ('RGBA', 'P') and fmt == 'JPEG':
+                            pil_img = pil_img.convert('RGB')
+                            
+                        pil_img.save(thumb_io, format=fmt, quality=85)
+                        thumb_io.seek(0)
+                        
+                        thumb_name = os.path.splitext(img.filename)[0] + "_thumb" + ext
+                        img.thumbnail.save(thumb_name, File(thumb_io), save=True)
+                        
+                self.stdout.write(self.style.SUCCESS(f"[{i}/{total}] Created thumbnail for {img.filename}"))
             except Exception as e:
                 self.stdout.write(self.style.ERROR(f"[{i}/{total}] Error on {img.id}: {e}"))
                 
